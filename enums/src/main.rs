8dd0de6189760a4557ad896dbab2172ba6e6686c
Xiao <xiao.wang@wework.com>
2020-11-10 18:03:32 +0800

    Add more examples for enum

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 enum Message {
     Quit,
     Move { x:i32, y:i32 },
@@ -10,10 +12,30 @@ impl Message {
         println!("I'm inside call!")
     }
 }
+
+enum Day {
+    Monday, Tuesday, Wednesday, Thursday, Friday,
+    Saturday, Sunday
+}
+
+impl Day {
+    fn is_weekday(&self) -> bool {
+        match self {
+            Day::Saturday | Day::Sunday => false,
+            _ => true
+        }
+    }
+}
+
 fn main() {
     let m = Message::Write(String::from("Hello"));
+    m.call();
 
-    m.call()
+    let day = Day::Sunday;
+    println!("Is the day a weekday? {}", day.is_weekday());
+
+    let day = Day::Tuesday;
+    println!("Is the day a weekday? {}", day.is_weekday());
 }
 
 
